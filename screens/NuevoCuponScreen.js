cd0bf7f2ffc0ca04df75486977c2c7998140708b
Anthony <meroanthony6@gmail.com>
2021-08-31 23:53:44 -0500

    errores logicosa agreagados, no custom

--- screens/NuevoCuponScreen.js
+++ screens/NuevoCuponScreen.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useEffect, useState,useCallback } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 
 import {
   View,
@@ -10,45 +10,70 @@ import {
 import { Title, Card, TextInput, Button } from 'react-native-paper';
 
 import { stylesApp } from '../const/styles.js';
-import {ErroresComponent} from '../componentes/ErroresComponent'
-import {ERRORS}from '../const/Errors.js'
-import {CREAR_CUPON} from '../const/Urls.js';
+import { ErroresComponent } from '../componentes/ErroresComponent'
+import { ERRORS } from '../const/Errors.js'
+import { CREAR_CUPON } from '../const/Urls.js';
+import { validateFieldRage } from '../utils/index.js';
 
 
-const NuevoCuponScreen = ({ navigation, menu, drawer,codNegocio }) => {
+const NuevoCuponScreen = ({ navigation, menu, drawer, codNegocio }) => {
   const [createCupon, setcreateCupon] = useState(false);
-  const [cuponInformation, setcuponInformation] = useState({titulo: "", descripcion: "", precio:"", precio_descuento: "",}) ;
-  const saveDataCupon=useCallback(
-    ()=>{
-      setcreateCupon(true);
+  const [cuponInformation, setcuponInformation] = useState({ titulo: "", descripcion: "", precio: "", precio_descuento: "", });
+  const [errores, setErrores] = useState([])
+
+  const saveDataCupon = useCallback(
+    () => {
+      const currentCuponInfo = Object.assign({}, cuponInformation);
+      const { titulo, descripcion, precio, precio_descuento } = currentCuponInfo;
+      const isValidateTitulo = validateFieldRage(titulo, 3, 25);
+      const isValidateDescripcion = validateFieldRage(descripcion, 10, 250);
+      const isValidatePrecio = validateFieldRage(precio, 1, 6);
+      const isValidatePrecioDescuento = validateFieldRage(precio_descuento, 1, 6);
+      const isAllCampsFills = isValidatePrecioDescuento && isValidatePrecio && isValidateDescripcion && isValidateTitulo;      
+      alert(isAllCampsFills);
+      if (!isAllCampsFills) {        
+        setErrores(prev => {
+          const haveThisError = prev.some(error => error.cod === ERRORS.nuevoCuponScreen.camposVacios.cod);
+          const currentErrores = [...prev, ERRORS.nuevoCuponScreen.camposVacios]
+          return haveThisError ? prev : currentErrores;
+        });
+      }else{        
+        setErrores(prev => {
+          const deleteError = prev.filter(error => error.cod !== ERRORS.nuevoCuponScreen.camposVacios.cod);
+          return deleteError;
+        });
+
+        setcreateCupon(true);
+      }      
       setTimeout(() => {
         setcreateCupon(false);
       }, 1000);
-    }
+
+    }, [cuponInformation]
   )
   useEffect(() => {
-      const crearCuponDB=async() =>{
-      
-        try {
-          const url=CREAR_CUPON;          
-          const dataCupon=Object.assign({negocioCodNegocio:codNegocio}, cuponInformation); 
-          const resultado=await axios.post(url,dataCupon);
-          console.log(resultado);
-          alert('Cupon creado con exito');
-        } catch (error) {
-          console.log(error);
-        }  
+    const crearCuponDB = async () => {
+
+      try {
+        const url = CREAR_CUPON;
+        const dataCupon = Object.assign({ negocioCodNegocio: codNegocio }, cuponInformation);
+        const resultado = await axios.post(url, dataCupon);
+        console.log(resultado);
+        alert('Cupon creado con exito');
+      } catch (error) {
+        console.log(error);
       }
-      createCupon?crearCuponDB():null;
+    }
+    createCupon ? crearCuponDB() : null;
   }, [createCupon]);
 
 
-  const setAttr=(attr,value)=>{
-    setcuponInformation(prev=>{
-      const currentObject=Object.assign({},prev);
-      currentObject[attr]=value;
+  const setAttr = (attr, value) => {
+    setcuponInformation(prev => {
+      const currentObject = Object.assign({}, prev);
+      currentObject[attr] = value;
       return currentObject;
-    });    
+    });
   }
 
   return (
@@ -57,60 +82,62 @@ const NuevoCuponScreen = ({ navigation, menu, drawer,codNegocio }) => {
       drawerPosition="left"
       drawerWidth={300}
       ref={drawer}>
-      
+
       <ScrollView>
-        
+
         <View style={stylesApp.container}>
-        
+
           <Title style={stylesApp.textTitle}>Nuevo Cupón</Title>
           <ErroresComponent
-        errores={[ERRORS.nuevoCuponScreen.camposVacios,ERRORS.nuevoCuponScreen.tipoDeDatoIncorrecto]}
-        />
-        
+            errores={errores}
+          />
+
           <Card>
             <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
           </Card>
           <TextInput
             mode="outlined"
             style={stylesApp.inputTextMarginTop}
-            label="Título" 
-            maxSize={50}            
-            onChangeText={(e) =>setAttr('titulo',e)}  
-            defaultValue={`${cuponInformation.titulo}`} 
-            />
-            
-            <TextInput
+            label="Título"
+            maxSize={50}
+            onChangeText={(e) => {
+              setAttr('titulo', e)
+            }}
+            defaultValue={`${cuponInformation.titulo}`}
+          />
+
+          <TextInput
             mode="outlined"
             style={stylesApp.inputTextMarginTop}
             label="Descripción"
             maxSize={250}
             multiline={true}
-            onChangeText={(e) =>setAttr('descripcion',e)}
+            onChangeText={(e) => setAttr('descripcion', e)}
             defaultValue={`${cuponInformation.descripcion}`}
-          />         
-        
+          />
+
           <TextInput
             mode="outlined"
             style={stylesApp.inputTextMarginTop}
             label="Precio Normal"
             keyboardType="number-pad"
             maxSize={8}
-            onChangeText={(e) =>setAttr('precio',e)}
+            onChangeText={(e) => setAttr('precio', e)}
             defaultValue={`${cuponInformation.precio}`}
           />
           <TextInput
-            mode="outlined"            
+            mode="outlined"
             style={stylesApp.inputTextMarginTop}
             maxSize={8}
             label="Precio con Descuento"
             keyboardType="number-pad"
-            onChangeText={(e) =>setAttr('precio_descuento',e)}
+            onChangeText={(e) => setAttr('precio_descuento', e)}
             defaultValue={`${cuponInformation.precio_descuento}`}
           />
           <View style={{ marginTop: 16 }}>
             <Button
               style={stylesApp.btnPrimaryCuadrado}
-              onPress={()=>saveDataCupon()}
+              onPress={() => saveDataCupon()}
               compact={true}
               mode="contained">
               <Text style={stylesApp.textButtons}>Crear Nuevo Cupón</Text>
