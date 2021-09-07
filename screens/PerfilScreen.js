dce0ecfc67ee40187350b5858ca0fc1bcff6b2e7
Luis Ariel <luis113818@hotmail.com>
2021-09-06 22:07:40 -0500

    revision burbano 2

--- screens/PerfilScreen.js
+++ screens/PerfilScreen.js
@@ -12,97 +12,101 @@ import {Avatar, Card, Button} from 'react-native-paper';
 import CuponNegocio from '../componentes/CuponNegocio';
 import ItemCiudad from '../componentes/ItemCiudad';
 import UltimaSucursal from '../componentes/UltimaSucursal';
-import { stylesApp} from '../const/styles.js';
-
-
+import {stylesApp} from '../const/styles.js';
+import Icon from 'react-native-vector-icons/Feather';
 
 const CIUDADES_LISTS = [
   {
-    id:1,
-    ciudad:'Quito',
-    cantidadSucursales:12,
+    id: 1,
+    ciudad: 'Quito',
+    cantidadSucursales: 12,
   },
   {
-    id:2,
-    ciudad:'Guayaquil',
-    cantidadSucursales:12,
+    id: 2,
+    ciudad: 'Guayaquil',
+    cantidadSucursales: 12,
   },
   {
-    id:3,
-    ciudad:'Loja',
-    cantidadSucursales:12,
+    id: 3,
+    ciudad: 'Loja',
+    cantidadSucursales: 12,
   },
   {
-    id:4,
-    ciudad:'Cuenca',
-    cantidadSucursales:12,
+    id: 4,
+    ciudad: 'Cuenca',
+    cantidadSucursales: 12,
   },
-]
+];
 
-const PerfilScreen = ({drawer, menu,navigation}) => {
-   const [cupon, setState] = useState([]);
+const PerfilScreen = ({drawer, menu, navigation}) => {
+  const [cupon, setState] = useState([]);
   function handleOnPress() {
     navigation.navigate('CuponScreen');
   }
 
   return (
-    <DrawerLayoutAndroid        
-      renderNavigationView={()=>menu}
-        drawerPosition='left'
-        drawerWidth={300}
-        ref={drawer}
-    >
-      
-        <ScrollView style={stylesApp.container}>
-          <Text style={stylesApp.textTitle}>MI PERFIL</Text>
-          <View style={{flex: 1}}>
-            <View style={{paddingTop: 16}}>
-                <Card style={styles.card}>
-                <Avatar.Image style={{alignSelf: 'center'}} size={107} source={require('../assets/avatar.jpg')}/>
-                <Text style={stylesApp.subTitle}>Luis Alfonso Hernadez Garcia</Text>
-                <Text style={stylesApp.subTitle}>RUC: 1722785642</Text>
-                </Card>
-                
-            </View>
+    <DrawerLayoutAndroid
+      renderNavigationView={() => menu}
+      drawerPosition="left"
+      drawerWidth={300}
+      ref={drawer}>
+      <ScrollView style={stylesApp.container}>
+        <View style={{display: 'flex', flexDirection: 'row'}}>
+          <View style={{width: '70%'}}>
+            <Text style={stylesApp.textTitle}>MI PERFIL</Text>
           </View>
-          <View style={{flex: 2, paddingTop:22, justifyContent: 'center'}}>
-            <Card style={styles.card}>
-                <Text style={stylesApp.subTitle}>Numero: 0978234522</Text>
-                <Text style={stylesApp.subTitle}>Correo: micorreo@gmail.com</Text>
-                <Text style={stylesApp.subTitle}>Pais: Ecuador</Text>
-                <Text style={stylesApp.subTitle}>Ciudad: Quito</Text>
-                <Text style={stylesApp.subTitle}>Contraseña: ************</Text>
-            </Card>
-     
-          </View>
-        <View style={{paddingTop:16}}>
-        <Button style={stylesApp.btnPrimaryCuadrado} mode='contained'>
-            Salir
-        </Button>
+          <Icon name="map-pin" size={24} color='#000000'/> 
         </View>
-    
-          
-        </ScrollView>
-      
+        <View style={{flex: 1}}>
+          <View style={{paddingTop: 16}}>
+            <Card style={styles.card}>
+              <Avatar.Image
+                style={{alignSelf: 'center'}}
+                size={107}
+                source={require('../assets/avatar.jpg')}
+              />
+              <Text style={stylesApp.subTitle}>
+                Luis Alfonso Hernadez Garcia
+              </Text>
+              <Text style={stylesApp.subTitle}>RUC: 1722785642</Text>
+            </Card>
+          </View>
+        </View>
+        <View style={{flex: 2, paddingTop: 22, justifyContent: 'center'}}>
+          <Card style={styles.card}>
+            <Text style={stylesApp.subTitle}>Numero: 0978234522</Text>
+            <Text style={stylesApp.subTitle}>Correo: micorreo@gmail.com</Text>
+            <Text style={stylesApp.subTitle}>Pais: Ecuador</Text>
+            <Text style={stylesApp.subTitle}>Ciudad: Quito</Text>
+            <Text style={stylesApp.subTitle}>Contraseña: ************</Text>
+          </Card>
+        </View>
+        <View style={{paddingTop: 16}}>
+          <Button style={stylesApp.btnPrimaryCuadrado} mode="contained">
+            Salir
+          </Button>
+        </View>
+      </ScrollView>
     </DrawerLayoutAndroid>
   );
 };
 
 const styles = StyleSheet.create({
-
   card: {
-  alignItems: 'center',
-  paddingVertical:16,
-  justifyContent: 'center'
+    alignItems: 'center',
+    paddingVertical: 16,
+    justifyContent: 'center',
   },
 });
 
 export default PerfilScreen;
 
-{/* <TouchableHighlight
+{
+  /* <TouchableHighlight
               onPress={() => navigation.navigate('SucursalScreen')}>
               <UltimaSucursal
                 nombreSucursal="Calzado de Pedro"
                 ciudadSucursal="Quito - Ecuador"
               />
-            </TouchableHighlight> */}
+            </TouchableHighlight> */
+}
